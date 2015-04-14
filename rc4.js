8b3252fa606ec39b046ac052dc1450278d8b2d34
Kon <knguyen6@uw.edu>
2015-04-13 19:46:05 -0700

    rc4 program added

--- rc4.js
+++ rc4.js
@@ -1,23 +1,58 @@
 var s = [],
+	cipher = [],
 	size=10;
-
 /* Key Schedule Algorithm */
 function doksa(key){
-	for(i=0; i<size; i++){
+	for(i=0; i<size; i++)
 		s.push(i);
-	}
-	var key_len = key.length;
-	for(j=0; j<size;j++){
-		j=(j+s[i] +key.charAt[i%key_len])%(size+1);
-		console.log(key.charAt[i]);
+	
+	var key_len = key.length,
+		j=0;
+	for(i=0; i<size;i++){
+		j=(j+s[i] +key.charAt(i%key_len).charCodeAt(0))%(size+1); 
+	
+	/*swapping s[i] and s[j]*/		
 		var temp = s[i];
 		s[i]=s[j];
 		s[j]=temp;
-		console.log('si = '+s[i]);
+	/* ==================== */
+		//console.log('si = '+s[i] + '  --------- sj = '+ s[j]);
 	}
-	return s;
+	console.log('KSA = '+s);
 }
-console.log(doksa('abc'));
-var rc4 = function(text, key){
+/* Psudorandom PRGA algorithm */
+function doprga(){
+	var i=0, 
+		j=0,
+		k=0;
+	while(k<size){
+		i = (i+1)%size;
+		j = (j+s[i])%size;
+
+	/*swapping s[i] and s[j]*/
+		var temp = s[i];
+		s[i]=s[j];
+		s[j]=temp;
+	/* ==================== */
+	cipher[k]=s[(s[i]+s[j])%size];
+	k++;
+	}
+	console.log('cipher= ' +cipher);
 
 }
+// console.log(doksa('abc'));
+// console.log(doprga());
+function rc4(text, key){
+	doksa(key);
+	doprga();
+	var cipherText=[];
+	for(i=0;i<text.length;i++){
+		var charToDec = text[i].charCodeAt(0);
+		var temp=cipher[i]^charToDec;
+		cipherText.push(temp);
+		console.log(temp);
+	}
+	return cipherText;
+}
+//console.log(String.fromCharCode('100')); to get char back from ascii value
+console.log(rc4('hello', 'key'));

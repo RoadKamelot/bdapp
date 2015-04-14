af1179a6423c5899890ce468ae59b6a0f16f2deb
Kon <knguyen6@uw.edu>
2015-04-13 22:56:13 -0700

    apply angular version of the hw

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ var express = require('express'),
 	path = require('path'),
 	app = express();
 
+app.use(express.static(__dirname+'/'));
 
 app.set('port', (process.env.PORT || 8080))
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/index1.html'));
 });
 
 app.listen(app.get('port'), function() {

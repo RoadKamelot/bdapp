16c0b42c18262e2ee0300d7a770ce7d55f8cd060
Kon <knguyen6@uw.edu>
2015-05-26 21:16:42 -0700

    team page

--- controller/controller.js
+++ controller/controller.js
@@ -13,8 +13,32 @@ bdapp.controller('navbarController', ['$scope', '$http', '$interval',function($s
 		}).error(function(){
 			console.log("Error while getting data from app.js to controller, cant get /temp from http in dronedbAccessor");
 		});
-	}
-	
+	};
+	$scope.sponsors = [{name: 'Bob Landowski',
+						role: 'Laboratory Support Engineer',
+						icon: './pictures/bob.jpg',
+						url: 'http://directory.tacoma.uw.edu/employee/ski0'},
+						{name: 'Orlando Baiocchi, Ph.D.',
+						role: 'Endowed Professor',
+						icon: './pictures/ob.jpg',
+						url: 'http://directory.tacoma.uw.edu/employee/baiocchi'}];
+
+	$scope.members = [{name: 'Chris Barrett',
+						major: 'CES',
+						icon: './pictures/Chris.PNG',
+						linkedin: 'https://www.linkedin.com/in/barrettcm'},
+						{name: 'Eric Yee',
+						major: 'CSS/CES',
+						icon: './pictures/Eric.PNG',
+						linkedin: 'https://www.linkedin.com/pub/eric-yee/92/61b/b3'},
+						{name: 'Khanh Nguyen',
+						major: 'CSS/CES ',
+						icon: './pictures/kon.jpg',
+						linkedin: 'https://www.linkedin.com/in/konnguyen6'},
+						{name: 'Brendon Crawford',
+						major: 'CES',
+						icon: './pictures/brendon.PNG',
+						linkedin: 'https://www.linkedin.com/pub/brendan-crawford/b9/461/79'} ];
 }]);
 
 function appConfig($routeProvider){

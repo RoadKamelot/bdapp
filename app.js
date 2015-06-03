861361e5c892d044897f186efc7472351eb080fb
Kon <knguyen6@uw.edu>
2015-06-02 21:28:05 -0700

    test db

--- app.js
+++ app.js
@@ -18,21 +18,20 @@ app.get('/', function(req, res) {
 /* Temperature*/
 app.get('/temp', function(req, res){
 	dronedbAccessor.temperatureData(function(err, result){
-		// var groupbyUnits = _.groupBy(result, 'Units');
-		// var final_result = [];
-		// for (var i = 0; i < groupbyUnits.degrees.length;i++){
-		// 	// console.log(groupbyUnits.Celsius[i]);
-		// 	var data = {
-		// 		Time: groupbyUnits.Celsius[i].SEN_TIME,
-		// 		Temp: groupbyUnits.Celsius[i].SEN_CALC || '0',
-		// 		Speed: groupbyUnits['Miles per Hour'][i].SEN_CALC || '0',
-		// 		Direction: groupbyUnits.degrees[i].SEN_CALC
-		// 	};
-		// 		final_result.push(data);
+		var groupbyUnits = _.groupBy(result, 'Units');
+		var final_result = [];
+		for (var i = 0; i < groupbyUnits.degrees.length;i++){
+			// console.log(groupbyUnits.Celsius[i]);
+			var data = {
+				Time: groupbyUnits.Celsius[i].SEN_TIME,
+				Temp: groupbyUnits.Celsius[i].SEN_CALC || '0',
+				Speed: groupbyUnits['Miles per Hour'][i].SEN_CALC || '0',
+				Direction: groupbyUnits.degrees[i].SEN_CALC
+			};
+				final_result.push(data);
 
-		// }
-		// return result ? res.send(final_result):res.send(err);
-		return res.send("Hello World --------------------------");
+		}
+		return result ? res.send(final_result):res.send(err);
 
 	});
 });
